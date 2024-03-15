b5070a6cae2745281abbcb0013a5240cd8313d72
Johnny Tran <tranjoh@students.zhaw.ch>
2024-03-15 14:37:20 +0100

    personalize

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to the demo project</h1>
-<p>This demo shows use cases of simple "KI-Anwendungen".</p>
+<h1>Johnny here, Welcome to the demo project</h1>
+<p>This demo shows use cases of simple "KI-Anwendungen", which we learnt in the KIA class at the ZHAW School.</p>
